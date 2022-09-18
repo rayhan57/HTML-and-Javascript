d9af2d5dcccfd138f1bff8b975a8b33287962207
rayhan57 <rayhanb18@gmail.com>
2022-09-18 17:49:19 +0700

    tes module

--- Dicoding/tiger.js
+++ Dicoding/tiger.js
@@ -1,10 +1,7 @@
-class Tiger {
-  constructor() {
-    this.strength = Math.floor(Math.random() * 100);
-  }
+const Tiger = () => {
+  const name = "Jesse";
+  const age = 40;
+  return name + " is " + age + "years old.";
+};
 
-  growl() {
-    return "grrrrrrr";
-  }
-}
 export default Tiger;
