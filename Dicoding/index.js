29c8c672f92f00d26a3270c64f500c87fd2f8fb5
rayhan57 <rayhanb18@gmail.com>
2022-09-18 21:02:50 +0700

    error handling

--- Dicoding/index.js
+++ Dicoding/index.js
@@ -1,2 +1,16 @@
-import Tiger from "./tiger";
-console.log(Tiger());
+class ValidationError extends Error {
+  constructor(message) {
+    this.name = message;
+  }
+}
+
+function validateNumberInput(pertama, kedua, ketiga) {
+  if (typeof pertama !== "number") {
+    console.log("Argumen pertama harus number");
+  } else if (typeof kedua !== "number") {
+    console.log("Argumen kedua harus number");
+  } else if (typeof ketiga !== "number") {
+    console.log("Argumen ketiga harus number");
+  }
+}
+console.log(validateNumberInput(3, 4, 5));
--- Dicoding/tiger.js
+++
@@ -1,7 +0,0 @@
-const Tiger = () => {
-  const name = "Jesse";
-  const age = 40;
-  return name + " is " + age + "years old.";
-};
-
-export default Tiger;
