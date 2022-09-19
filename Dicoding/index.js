f3c33abcdb3669f55d82bf26fa1f5ac60f971952
rayhan57 <rayhanb18@gmail.com>
2022-09-19 11:29:14 +0700

    Penanganan Eror

--- Dicoding/index.js
+++ Dicoding/index.js
@@ -1,16 +1,37 @@
 class ValidationError extends Error {
   constructor(message) {
-    this.name = message;
+    super(message);
+    this.name = "ValidationError";
   }
 }
 
-function validateNumberInput(pertama, kedua, ketiga) {
-  if (typeof pertama !== "number") {
-    console.log("Argumen pertama harus number");
-  } else if (typeof kedua !== "number") {
-    console.log("Argumen kedua harus number");
-  } else if (typeof ketiga !== "number") {
-    console.log("Argumen ketiga harus number");
+const validateNumberInput = (pertama, kedua, ketiga) => {
+  try {
+    if (typeof pertama !== "number") {
+      throw new ValidationError("Argumen pertama harus number");
+    } else if (typeof kedua !== "number") {
+      throw new ValidationError("Argumen kedua harus number");
+    } else if (typeof ketiga !== "number") {
+      throw new ValidationError("Argumen ketiga harus number");
+    }
+  } catch (err) {
+    console.log(`Terjadi kesalahan ${err.message}`);
   }
-}
-console.log(validateNumberInput(3, 4, 5));
+};
+
+// console.log(validateNumberInput(3, 4, 5));
+
+const detectTriangle = (a, b, c) => {
+  try {
+    validateNumberInput(2, 2, 2);
+    if (a === b && b === c) {
+      return "Segitiga sama sisi";
+    }
+    if (a === b || a === c || b === c) {
+      return "Segitiga sama kaki";
+    }
+  } catch (err) {
+    console.log(`Terjadi kesalahan ${err.message}`);
+  }
+  return "Segitiga sembarang";
+};
