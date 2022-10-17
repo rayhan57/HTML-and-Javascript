0d02576507ee559d80d0f435885e9e8cba9bce73
rayhan57 <rayhanb18@gmail.com>
2022-10-17 16:01:32 +0700

    ujian js

--- Dicoding/index.js
+++ Dicoding/index.js
@@ -1,14 +1,13 @@
-function findMin(a, b, c) {
-  let minValue = 0;
-
-  if (a < b) {
-    minValue = a;
-  } else if (b < c) {
-    minValue = b;
-  } else {
-    minValue = c;
-  }
-
-  return minValue;
+function fetchUsername() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("JSUser");
+    }, 3000);
+  });
 }
-console.log(findMin(2, 5, 7));
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+  console.log(`You are logged in as ${value}`);
+});
+console.log("Welcome!");
