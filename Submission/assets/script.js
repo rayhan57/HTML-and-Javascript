b9efe9d3d6d5cad70c0cae6e55dbb5c31e308612
rayhan57 <rayhanb18@gmail.com>
2022-11-04 10:17:15 +0700

    delete submission

--- Submission/assets/script.js
+++
@@ -1,8 +0,0 @@
-const navLink = document.querySelectorAll("a");
-
-navLink.forEach((link) => {
-  link.addEventListener("click", function () {
-    navLink.forEach((e) => e.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
